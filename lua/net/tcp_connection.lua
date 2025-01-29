b35257fb8ba863f9c0ae68aae6f184690e941cc7
Stefan Fenzke <stefan.fezke@gmx.de>
2025-01-29 01:18:10 +0100

    Implemented TCP connection

---
+++ lua/net/tcp_connection.lua
@@ -0,0 +1,26 @@
+local tcp_connection = {}
+--TODO: Callback for response data
+
+tcp_connection.send = function(ip, port, data, callback)
+	local tcp_handler = vim.uv.new_tcp()
+
+	if not tcp_handler then
+		return nil
+	end
+
+	tcp_handler:connect(ip, port, function(err)
+		-- TODO: Error handling
+		tcp_handler:write(data)
+		tcp_handler:read_start(function(read_error, chunk)
+			if read_error then
+				--TODO: Error handling
+			elseif chunk then
+				callback(chunk)
+			else
+				tcp_handler:close()
+			end
+		end)
+	end)
+end
+
+return tcp_connection
