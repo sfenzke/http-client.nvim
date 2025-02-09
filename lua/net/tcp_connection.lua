1e0faac31057dbb19aa89b273980660c94db366d
Stefan Fenzke <stefan.fenzke@hochschulstart.de>
2025-02-09 23:37:45 +0100

    added support to try a list of addresses

--- lua/net/tcp_connection.lua
+++ lua/net/tcp_connection.lua
@@ -1,44 +1,81 @@
 local tcp_connection = {}
---TODO: Callback for response data
 
--- doesnt suupport ipv6 right now
 tcp_connection.send = function(host, port, data, callback)
-	local tcp_handler = vim.uv.new_tcp()
-
-	if not tcp_handler then
-		return nil
-	end
-
 	local response = {}
 
-	vim.uv.getaddrinfo(host, port, { protocol = "tcp", family = "inet" }, function(name_resolution_error, addresses)
-		if name_resolution_error then
-			return
-		end
-
-		-- TODO: implement multiple ip adresses
-		tcp_handler:connect(addresses[1].addr, port, function(network_error)
-			if network_error then
-				-- TODO: improve error handling
+	-- Resolve hostname to an IP address
+	vim.uv.getaddrinfo(host, tostring(port), { protocol = "tcp", family = "inet" },
+		function(name_resolution_error, addresses)
+			if name_resolution_error then
+				if callback then callback("DNS resolution error: " .. name_resolution_error, nil) end
 				return
 			end
 
-			tcp_handler:write(data)
-			tcp_handler:read_start(function(read_error, chunk)
-				if read_error then
-					callback(read_error, nil)
-				elseif chunk then
-					table.insert(response, chunk)
-				else
-					-- if we reached this point it means the reponse has been received in full
-					tcp_handler:close()
-					if callback then
-						callback(nil, table.concat(response, ""))
-					end
+			if not addresses or #addresses == 0 then
+				if callback then callback("No addresses found for host", nil) end
+				return
+			end
+
+			-- Function to attempt connection to each address sequentially
+			local function try_connect(index)
+				if index > #addresses then
+					if callback then callback("Failed to connect to any resolved address", nil) end
+					return
 				end
-			end)
+
+				local addr = addresses[index].addr
+
+				-- Create a new TCP handle for each attempt
+				local tcp_handler = vim.uv.new_tcp()
+				if not tcp_handler then
+					if callback then callback("Failed to create TCP handle", nil) end
+					return
+				end
+
+				-- Attempt connection
+				tcp_handler:connect(addr, port, function(network_error)
+					if network_error then
+						-- Close the failed handler before retrying
+						tcp_handler:close()
+						try_connect(index + 1) -- Try the next address
+						return
+					end
+
+					-- Successfully connected, send data
+					tcp_handler:write(data, function(write_error)
+						if write_error then
+							tcp_handler:close()
+							if callback then
+								callback("Failed to send data: " .. write_error,
+									nil)
+							end
+							return
+						end
+
+						-- Start reading response
+						tcp_handler:read_start(function(read_error, chunk)
+							if read_error then
+								tcp_handler:close()
+								if callback then
+									callback("Read error: " .. read_error,
+										nil)
+								end
+								return
+							elseif chunk then
+								table.insert(response, chunk)
+							else
+								-- Reading finished, call the callback
+								tcp_handler:close()
+								if callback then callback(nil, table.concat(response, "")) end
+							end
+						end)
+					end)
+				end)
+			end
+
+			-- Start trying connections
+			try_connect(1)
 		end)
-	end)
 end
 
 return tcp_connection
